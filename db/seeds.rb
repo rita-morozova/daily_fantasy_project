a2f36982ca31ff2320207913ecbf3549269c52bd
MullaN <admullan@gmail.com>
2020-09-29 16:04:40 -0700

    added week seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,13 @@ teams = FFNerd.teams
 players.select! {|player| player.active == "1" }
 players.select! {|player| player.position != "K" && player.position != "DEF" }
 players.each {|player| Player.create(name: player[:displayName], position: player[:position], nfl_team_code: player[:team], nfl_team_name: teams.find {|team| team.code == player[:team]}.full_name)}
+completed_game_weeks = 3
+(1..completed_game_weeks).each do |week|
+    table = CSV.parse(File.read("storage/points_week_#{week}.csv"), headers: true)
+    table.each do |row|
+        player = Player.find_by(name: row[1])
+        if player
+            Week.create(player_id: player.id, game_week: week, score: row[4])
+        end
+    end
+end
